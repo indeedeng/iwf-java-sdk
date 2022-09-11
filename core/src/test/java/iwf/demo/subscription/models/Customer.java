770f9c291051a674608c374639b0e781865e2c71
duoertai <duoertai@gmail.com>
2022-09-10 20:29:45 -0700

    copy demo workflow over

---
+++ core/src/test/java/iwf/demo/subscription/models/Customer.java
@@ -0,0 +1,11 @@
+package iwf.demo.subscription.models;
+
+public class Customer {
+    public String getId() {
+        return null;
+    }
+
+    public Subscription getSubscription() {
+        return null;
+    }
+}
