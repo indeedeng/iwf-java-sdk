62c71bfee63e6e5ed9f10932d1faede5752c5c2c
Quanzheng Long <qlong@indeed.com>
2022-09-27 16:13:19 -0700

    use java generator

--- core/src/main/java/iwf/core/spring/controller/ApiController.java
+++ core/src/main/java/iwf/core/spring/controller/ApiController.java
@@ -1,29 +1,23 @@
 package iwf.core.spring.controller;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import iwf.gen.api.ApiApi;
 import iwf.gen.models.WorkflowStateStartRequest;
 import iwf.gen.models.WorkflowStateStartResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
-
 @Controller
-public class ApiController implements ApiApi {
+public class ApiController {
 
     @RequestMapping("/")
     public ResponseEntity<String> index() {
         return ResponseEntity.ok("Hello, world!");
     }
 
-    @Override
     public ResponseEntity<WorkflowStateStartResponse> apiV1WorkflowStateStartPost(
-             final WorkflowStateStartRequest workflowStateStartRequest
+            final WorkflowStateStartRequest workflowStateStartRequest
     ) {
-        System.out.println("this is a test log"+ workflowStateStartRequest.toString());
+        System.out.println("this is a test log" + workflowStateStartRequest.toString());
         return null;
     }
 
