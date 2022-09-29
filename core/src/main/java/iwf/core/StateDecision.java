b41883bbba20f1a62b854f117191e42acd4f5003
Quanzheng Long <qlong@indeed.com>
2022-09-29 14:44:27 -0700

    Remove unused field in StateDecision

--- core/src/main/java/iwf/core/StateDecision.java
+++ core/src/main/java/iwf/core/StateDecision.java
@@ -3,7 +3,6 @@ package iwf.core;
 import org.immutables.value.Value;
 
 import java.util.List;
-import java.util.Map;
 
 import static iwf.core.StateMovement.COMPLETING_WORKFLOW_MOVEMENT;
 import static iwf.core.StateMovement.FAILING_WORKFLOW_MOVEMENT;
@@ -13,14 +12,11 @@ public interface StateDecision {
 
     List<StateMovement> getNextStates();
 
-    Map<String, Object> getUpsertSearchAttributes();
-
-    Map<String, Object> getUpsertQueryAttributes();
-
     boolean getWaitForMoreCommandResults();
 
+    StateDecision NO_NEXT = ImmutableStateDecision.builder().build();
     StateDecision COMPLETING_WORKFLOW = ImmutableStateDecision.builder().addNextStates(COMPLETING_WORKFLOW_MOVEMENT).build();
     StateDecision FAILING_WORKFLOW = ImmutableStateDecision.builder().addNextStates(FAILING_WORKFLOW_MOVEMENT).build();
 
-    StateDecision DECISION_WAIT_FOR_MORE_RESULTS = ImmutableStateDecision.builder().waitForMoreCommandResults(true).build();
+    StateDecision WAIT_FOR_MORE_RESULTS = ImmutableStateDecision.builder().waitForMoreCommandResults(true).build();
 }
