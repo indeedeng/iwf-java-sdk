94ece767daf4413673b692295e4a7e5f115b22ab
Quanzheng Long <qlong@indeed.com>
2022-10-07 15:28:28 -0700

    Some short cut commands for timer and signal

--- core/src/main/java/iwf/core/command/SignalCommand.java
+++ core/src/main/java/iwf/core/command/SignalCommand.java
@@ -6,4 +6,17 @@ import org.immutables.value.Value;
 public abstract class SignalCommand implements BaseCommand {
 
     public abstract String getSignalName();
+
+    public static SignalCommand create(final String commandId, final String signalName) {
+        return ImmutableSignalCommand.builder()
+                .signalName(signalName)
+                .commandId(commandId)
+                .build();
+    }
+
+    public static SignalCommand create(String signalName) {
+        return ImmutableSignalCommand.builder()
+                .signalName(signalName)
+                .build();
+    }
 }
