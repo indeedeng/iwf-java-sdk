e353cb2eda3d22210d0c49b23d1e1f6932c6e968
Quanzheng Long <qlong@indeed.com>
2022-10-07 15:50:40 -0700

    merge

--- core/src/test/java/iwf/integ/basic/BasicWorkflow.java
+++ core/src/test/java/iwf/integ/basic/BasicWorkflow.java
@@ -2,11 +2,21 @@ package iwf.integ.basic;
 
 import iwf.core.StateDef;
 import iwf.core.Workflow;
+import iwf.core.command.SignalMethodDef;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class BasicWorkflow implements Workflow {
+
+    public static final String TEST_SIGNAL_NAME = "test-signal-name";
+
+    @Override
+    public List<SignalMethodDef<?>> getSignalMethods() {
+        return Arrays.asList(SignalMethodDef.create(String.class, TEST_SIGNAL_NAME));
+    }
+
+    @Override
     public List<StateDef> getStates() {
         return Arrays.asList(
                 StateDef.startingState(new BasicWorkflowS1()),
