d3142ae61bc5be4fd424b04642a6d9ccbe3ef728
Quanzheng Long <qlong@indeed.com>
2022-11-08 13:50:11 -0800

    Add client option for docker

--- src/main/java/io/github/cadenceoss/iwf/core/ClientOptions.java
+++ src/main/java/io/github/cadenceoss/iwf/core/ClientOptions.java
@@ -9,9 +9,14 @@ public abstract class ClientOptions {
     public abstract String getWorkerUrl();
 
     public static final String defaultWorkerUrl = "http://localhost:8802";
+
+    public static final String workerUrlFromDocker = "http://host.docker.internal:8802";
     public static final String defaultServerUrl = "http://localhost:8801";
 
     public static final ClientOptions localDefault = minimum(defaultWorkerUrl, defaultServerUrl);
+
+    // use this when running with docker-compose of iWF server
+    public static final ClientOptions dockerDefault = minimum(defaultWorkerUrl, defaultServerUrl);
 
     public static ClientOptions minimum(final String workerUrl, final String serverUrl) {
         return ImmutableClientOptions.builder().workerUrl(workerUrl).serverUrl(serverUrl).build();
