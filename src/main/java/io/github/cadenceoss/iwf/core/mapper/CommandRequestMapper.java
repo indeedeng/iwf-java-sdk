3c2268810e6fd8273ff9fa30a4578a31ccc89a80
Quanzheng Long <longquanzheng@users.noreply.github.com>
2022-12-11 21:24:28 -0800

    Reduce JSON size for worker API response (#76)

--- src/main/java/io/github/cadenceoss/iwf/core/mapper/CommandRequestMapper.java
+++ src/main/java/io/github/cadenceoss/iwf/core/mapper/CommandRequestMapper.java
@@ -1,35 +1,45 @@
 package io.github.cadenceoss.iwf.core.mapper;
 
 import io.github.cadenceoss.iwf.core.command.CommandRequest;
+import io.github.cadenceoss.iwf.core.command.TimerCommand;
 import io.github.cadenceoss.iwf.core.communication.InterStateChannelCommand;
 import io.github.cadenceoss.iwf.core.communication.SignalCommand;
-import io.github.cadenceoss.iwf.core.command.TimerCommand;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class CommandRequestMapper {
     public static io.github.cadenceoss.iwf.gen.models.CommandRequest toGenerated(CommandRequest commandRequest) {
-        return new io.github.cadenceoss.iwf.gen.models.CommandRequest()
-                .signalCommands(
-                        commandRequest.getCommands().stream()
-                                .filter(baseCommand -> baseCommand instanceof SignalCommand)
-                                .map(baseCommand -> (SignalCommand) baseCommand)
-                                .map(SignalCommandMapper::toGenerated)
-                                .collect(Collectors.toList())
-                ).timerCommands(
-                        commandRequest.getCommands().stream()
-                                .filter(baseCommand -> baseCommand instanceof TimerCommand)
-                                .map(baseCommand -> (TimerCommand) baseCommand)
-                                .map(TimerCommandMapper::toGenerated)
-                                .collect(Collectors.toList())
-                )
-                .interStateChannelCommands(
-                        commandRequest.getCommands().stream()
-                                .filter(baseCommand -> baseCommand instanceof InterStateChannelCommand)
-                                .map(baseCommand -> (InterStateChannelCommand) baseCommand)
-                                .map(InterStateChannelCommandMapper::toGenerated)
-                                .collect(Collectors.toList())
-                )
+
+        final List<io.github.cadenceoss.iwf.gen.models.SignalCommand> signalCommands = commandRequest.getCommands().stream()
+                .filter(baseCommand -> baseCommand instanceof SignalCommand)
+                .map(baseCommand -> (SignalCommand) baseCommand)
+                .map(SignalCommandMapper::toGenerated)
+                .collect(Collectors.toList());
+
+        final List<io.github.cadenceoss.iwf.gen.models.TimerCommand> timerCommands = commandRequest.getCommands().stream()
+                .filter(baseCommand -> baseCommand instanceof TimerCommand)
+                .map(baseCommand -> (TimerCommand) baseCommand)
+                .map(TimerCommandMapper::toGenerated)
+                .collect(Collectors.toList());
+
+        final List<io.github.cadenceoss.iwf.gen.models.InterStateChannelCommand> interstateChannelCommands = commandRequest.getCommands().stream()
+                .filter(baseCommand -> baseCommand instanceof InterStateChannelCommand)
+                .map(baseCommand -> (InterStateChannelCommand) baseCommand)
+                .map(InterStateChannelCommandMapper::toGenerated)
+                .collect(Collectors.toList());
+
+        final io.github.cadenceoss.iwf.gen.models.CommandRequest commandRequestResults = new io.github.cadenceoss.iwf.gen.models.CommandRequest()
                 .deciderTriggerType(commandRequest.getDeciderTriggerType());
+        if (signalCommands.size() > 0) {
+            commandRequestResults.signalCommands(signalCommands);
+        }
+        if (timerCommands.size() > 0) {
+            commandRequestResults.timerCommands(timerCommands);
+        }
+        if (interstateChannelCommands.size() > 0) {
+            commandRequestResults.interStateChannelCommands(interstateChannelCommands);
+        }
+        return commandRequestResults;
     }
 }
