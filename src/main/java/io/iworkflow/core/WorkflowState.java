179be69bf739c54049358ea0f508f542b023928e
Quanzheng Long <qlong@indeed.com>
2022-12-13 13:43:05 -0800

    Fix comment text

--- src/main/java/io/iworkflow/core/WorkflowState.java
+++ src/main/java/io/iworkflow/core/WorkflowState.java
@@ -22,14 +22,14 @@ public interface WorkflowState<I> {
      * Implement this method to execute the commands set up for this state.
      *
      * @param context       the context info of this API invocation, like workflow start time, workflowId, etc
-     * @param input         the state input which is deserialized by dataConverter with {@link #getInputType}
+     * @param input         the state input which is deserialized by {@link ObjectEncoder} with {@link #getInputType}
      * @param persistence   persistence API for 1) data objects, 2) search attributes and 3) stateLocals 4) recordEvent
-     *                      DataObjects and SearchAttributes are defined by {@link Workflow} interface.    
+     *                      DataObjects and SearchAttributes are defined by {@link Workflow} interface.
      *                      StateLocals are for passing data within the state execution from this start API to {@link #decide} API
-     *                      RecordEvent is for storing some tracking info(e.g. RPC call input/output) when executing the API.     
-     *                      Note that the write API will be recorded to server after the whole start API response is accepted.
+     *                      RecordEvent is for storing some tracking info(e.g. RPC call input/output) when executing the API.
+     *                      Note that any write API will be recorded to server after the whole start API response is accepted.
      * @param communication communication API, right now only for publishing value to interstate channel
-     *                      Note that the write API will be recorded to server after the whole start API response is accepted.
+     *                      Note that any write API will be recorded to server after the whole start API response is accepted.
      * @return the requested commands for this step
      */
     CommandRequest start(
@@ -40,16 +40,16 @@ public interface WorkflowState<I> {
     /**
      * Implement this method to decide what to do next when requested commands are ready
      *
-     * @param context          the context info of this API invocation, like workflow start time, workflowId, etc
-     * @param input            the state input which is deserialized by dataConverter with {@link #getInputType}
-     * @param commandResults   the results of the command that executed by {@link #start}
-     * @param persistence      persistence API for 1) data objects, 2) search attributes and 3) stateLocals 4) recordEvent
-     *                         DataObjects and SearchAttributes are defined by {@link Workflow} interface.    
-     *                         StateLocals are for passing data within the state execution from this start API to {@link #decide} API
-     *                         RecordEvent is for storing some tracking info(e.g. RPC call input/output) when executing the API.     
-     *                         Note that the write API will be recorded to server after the whole start API response is accepted.     
-     * @param communication    communication API, right now only for publishing value to interstate channel
-     *                         Note that the write API will be recorded to server after the whole decide API response is accepted.
+     * @param context        the context info of this API invocation, like workflow start time, workflowId, etc
+     * @param input          the state input which is deserialized by {@link ObjectEncoder} with {@link #getInputType}
+     * @param commandResults the results of the command that executed by {@link #start}
+     * @param persistence    persistence API for 1) data objects, 2) search attributes and 3) stateLocals 4) recordEvent
+     *                       DataObjects and SearchAttributes are defined by {@link Workflow} interface.
+     *                       StateLocals are for passing data within the state execution from this start API to {@link #decide} API
+     *                       RecordEvent is for storing some tracking info(e.g. RPC call input/output) when executing the API.
+     *                       Note that the write API will be recorded to server after the whole start API response is accepted.
+     * @param communication  communication API, right now only for publishing value to interstate channel
+     *                       Note that the write API will be recorded to server after the whole decide API response is accepted.
      * @return the decision of what to do next(e.g. transition to next states)
      */
     StateDecision decide(
