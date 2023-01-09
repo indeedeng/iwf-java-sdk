a0eeb928cb63696bde3623473d266bd75d4bc924
Quanzheng Long <qlong@indeed.com>
2023-01-09 09:26:26 -0800

    stash refactor

--- src/main/java/io/iworkflow/core/WorkflowOptions.java
+++ src/main/java/io/iworkflow/core/WorkflowOptions.java
@@ -1,12 +1,10 @@
 package io.iworkflow.core;
 
 import io.iworkflow.gen.models.RetryPolicy;
-import io.iworkflow.gen.models.SearchAttribute;
 import io.iworkflow.gen.models.WorkflowIDReusePolicy;
-import io.iworkflow.gen.models.WorkflowStateOptions;
 import org.immutables.value.Value;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Value.Immutable
@@ -17,13 +15,7 @@ public abstract class WorkflowOptions {
 
     public abstract Optional<String> getCronSchedule();
 
-    public abstract Optional<WorkflowStateOptions> getStartStateOptions();
-
     public abstract Optional<RetryPolicy> getWorkflowRetryPolicy();
 
-    public abstract Optional<List<SearchAttribute>> getInitialSearchAttribute();
-
-    public static WorkflowOptions minimum(final int workflowTimeoutSeconds) {
-        return ImmutableWorkflowOptions.builder().workflowTimeoutSeconds(workflowTimeoutSeconds).build();
-    }
+    public abstract Optional<Map<String, Object>> getInitialSearchAttribute();
 }
