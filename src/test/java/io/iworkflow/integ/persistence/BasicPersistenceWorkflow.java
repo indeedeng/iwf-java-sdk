53f06713467167d9e646fef1230439528b973e96
Quanzheng Long <qlong@indeed.com>
2023-01-20 09:19:35 -0800

    Fix assignment check for getDataObject

--- src/test/java/io/iworkflow/integ/persistence/BasicPersistenceWorkflow.java
+++ src/test/java/io/iworkflow/integ/persistence/BasicPersistenceWorkflow.java
@@ -5,7 +5,9 @@ import io.iworkflow.core.Workflow;
 import io.iworkflow.core.persistence.DataObjectDef;
 import io.iworkflow.core.persistence.PersistenceFieldDef;
 import io.iworkflow.core.persistence.SearchAttributeDef;
+import io.iworkflow.gen.models.Context;
 import io.iworkflow.gen.models.SearchAttributeValueType;
+import io.iworkflow.integ.basic.FakContextImpl;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,6 +16,9 @@ import java.util.List;
 @Component
 public class BasicPersistenceWorkflow implements Workflow {
     public static final String TEST_DATA_OBJECT_KEY = "data-obj-1";
+    public static final String TEST_DATA_OBJECT_MODEL_1 = "data-obj-2";
+
+    public static final String TEST_DATA_OBJECT_MODEL_2 = "data-obj-3";
 
     public static final String TEST_SEARCH_ATTRIBUTE_KEYWORD = "CustomKeywordField";
     public static final String TEST_SEARCH_ATTRIBUTE_INT = "CustomIntField";
@@ -27,6 +32,8 @@ public class BasicPersistenceWorkflow implements Workflow {
     public List<PersistenceFieldDef> getPersistenceSchema() {
         return Arrays.asList(
                 DataObjectDef.create(String.class, TEST_DATA_OBJECT_KEY),
+                DataObjectDef.create(Context.class, TEST_DATA_OBJECT_MODEL_1),
+                DataObjectDef.create(FakContextImpl.class, TEST_DATA_OBJECT_MODEL_2),
                 SearchAttributeDef.create(SearchAttributeValueType.INT, TEST_SEARCH_ATTRIBUTE_INT),
                 SearchAttributeDef.create(SearchAttributeValueType.KEYWORD, TEST_SEARCH_ATTRIBUTE_KEYWORD)
         );
--- src/test/java/io/iworkflow/integ/persistence/BasicPersistenceWorkflowState1.java
+++ src/test/java/io/iworkflow/integ/persistence/BasicPersistenceWorkflowState1.java
@@ -9,10 +9,13 @@ import io.iworkflow.core.command.CommandRequest;
 import io.iworkflow.core.command.CommandResults;
 import io.iworkflow.core.communication.Communication;
 import io.iworkflow.core.persistence.Persistence;
+import io.iworkflow.integ.basic.FakContextImpl;
 
 import java.util.Arrays;
 
 import static io.iworkflow.integ.persistence.BasicPersistenceWorkflow.TEST_DATA_OBJECT_KEY;
+import static io.iworkflow.integ.persistence.BasicPersistenceWorkflow.TEST_DATA_OBJECT_MODEL_1;
+import static io.iworkflow.integ.persistence.BasicPersistenceWorkflow.TEST_DATA_OBJECT_MODEL_2;
 import static io.iworkflow.integ.persistence.BasicPersistenceWorkflow.TEST_SEARCH_ATTRIBUTE_INT;
 import static io.iworkflow.integ.persistence.BasicPersistenceWorkflow.TEST_SEARCH_ATTRIBUTE_KEYWORD;
 
@@ -32,6 +35,15 @@ public class BasicPersistenceWorkflowState1 implements WorkflowState<String> {
     @Override
     public CommandRequest start(Context context, String input, Persistence persistence, final Communication communication) {
         persistence.setDataObject(TEST_DATA_OBJECT_KEY, "query-start");
+
+        final io.iworkflow.gen.models.Context fkCtx = new FakContextImpl();
+        // it's allowed to set a child class to a parent
+        persistence.setDataObject(TEST_DATA_OBJECT_MODEL_1, fkCtx);
+
+        // but it's not allowed to set a parent to child
+        //persistence.setDataObject(TEST_DATA_OBJECT_MODEL_2, new io.iworkflow.gen.models.Context());
+        persistence.setDataObject(TEST_DATA_OBJECT_MODEL_2, fkCtx);
+
         persistence.setStateLocal("test-key", "test-value-1");
         persistence.recordStateEvent("event-1", "event-1");
         persistence.recordStateEvent("event-2", "event-1", 2, "event-3");
@@ -44,6 +56,12 @@ public class BasicPersistenceWorkflowState1 implements WorkflowState<String> {
     public StateDecision decide(Context context, String input, CommandResults commandResults, Persistence persistence, final Communication communication) {
         String str = persistence.getDataObject(TEST_DATA_OBJECT_KEY, String.class);
         persistence.setDataObject(TEST_DATA_OBJECT_KEY, str + "-query-decide");
+
+        // it's not allowed to assign a parent to child
+        persistence.getDataObject(TEST_DATA_OBJECT_MODEL_1, io.iworkflow.gen.models.Context.class);
+        // but it's allowed to assign child to parent
+        persistence.getDataObject(TEST_DATA_OBJECT_MODEL_2, io.iworkflow.gen.models.Context.class);
+
         String testVal2 = persistence.getStateLocal("test-key", String.class);
         if (testVal2.equals("test-value-1")) {
             persistence.setStateLocal("test-key", "test-value-2");
