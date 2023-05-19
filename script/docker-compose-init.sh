6243726cc344bfce804e7e60758ed9fd3b65d2b7
Quanzheng Long <qlong@indeed.com>
2023-05-19 16:11:37 -0700

    Fix CI Search attribute registration

--- script/docker-compose-init.sh
+++ script/docker-compose-init.sh
@@ -13,8 +13,7 @@ fi
 }
 
 echo "now trying to register iWF system search attributes..."
-
-for run in {1..60}; do
+for run in {1..120}; do
   sleep 1
   tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
   sleep 0.1
@@ -22,12 +21,24 @@ for run in {1..60}; do
   sleep 0.1
   tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
   sleep 0.1
-  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" ] ; then
-      echo "All search attributes are registered"
-      break
-    fi
+  tctl search-attribute  create -name CustomKeywordField -type Keyword -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomIntField -type Int -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomBoolField -type Bool -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomDoubleField -type Double -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomDatetimeField -type Datetime -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomStringField -type text -y
+
+  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" && checkExists "CustomKeywordField" && checkExists "CustomIntField" && checkExists "CustomBoolField" && checkExists "CustomDoubleField" && checkExists "CustomDatetimeField" && checkExists "CustomStringField" ] ; then
+    echo "All search attributes are registered"
+    break
+  fi
+
 done
 
-tctl namespace register default
 
 tail -f /dev/null
