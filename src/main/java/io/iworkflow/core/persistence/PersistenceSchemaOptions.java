4e2ad2c63e2a4825f5af89fdc7228d0d14bf8eb1
Quanzheng Long <qlong@indeed.com>
2023-06-02 20:36:29 -0700

    fix comment

--- src/main/java/io/iworkflow/core/persistence/PersistenceSchemaOptions.java
+++ src/main/java/io/iworkflow/core/persistence/PersistenceSchemaOptions.java
@@ -7,7 +7,7 @@ public abstract class PersistenceSchemaOptions {
     // this option will enable caching data attributes as memo
     // the RPC/GetDataAttributes API will support a much higher throughput
     // However:
-    // 1. The read after write will become eventual consistent, unless set strongConsistencyReadWithCaching=true in RPC annotation
+    // 1. The read after write will become eventual consistent, unless set bypassCachingForStrongConsistency=true in RPC annotation
     // 2. Only iWF service with Temporal as backend is supporting this feature at the moment
     // 3. It will extra cost as it will upsertMemo(WorkflowPropertiesModified event in the history) for write
     public abstract boolean getCachingDataAttributesByMemo();
