084bda0e08aaec5dba812033ae6e610887039445
Quanzheng Long <qlong@indeed.com>
2023-07-05 15:41:18 -0700

    Add better support for wait timeout with subStatus

--- src/test/java/io/iworkflow/integ/WorkflowUncompletedTest.java
+++ src/test/java/io/iworkflow/integ/WorkflowUncompletedTest.java
@@ -1,9 +1,7 @@
 package io.iworkflow.integ;
 
-import io.iworkflow.core.Client;
-import io.iworkflow.core.ClientOptions;
-import io.iworkflow.core.ImmutableStopWorkflowOptions;
-import io.iworkflow.core.WorkflowUncompletedException;
+import io.iworkflow.core.*;
+import io.iworkflow.gen.models.ErrorSubStatus;
 import io.iworkflow.gen.models.WorkflowErrorType;
 import io.iworkflow.gen.models.WorkflowStatus;
 import io.iworkflow.gen.models.WorkflowStopType;
@@ -33,22 +31,27 @@ public class WorkflowUncompletedTest {
         final Client client = new Client(WorkflowRegistry.registry, ClientOptions.localDefault);
         final String wfId = "testWorkflowTimeout" + System.currentTimeMillis() / 1000;
         final Integer input = 1;
-        final String runId = client.startWorkflow(
-                BasicSignalWorkflow.class, wfId, 350, input);
 
+
+        client.startWorkflow(BasicSignalWorkflow.class, wfId, 100, input);
+
+
+        long startMs = System.currentTimeMillis();
+        long elapsedMs;
         try {
             client.getSimpleWorkflowResultWithWait(Integer.class, wfId);
-        } catch (WorkflowUncompletedException e) {
-            Assertions.assertEquals(runId, e.getRunId());
-            Assertions.assertEquals(WorkflowStatus.TIMEOUT, e.getClosedStatus());
-            Assertions.assertNull(e.getErrorSubType());
-            Assertions.assertNull(e.getErrorMessage());
-            Assertions.assertEquals(0, e.getStateResultsSize());
+        } catch (ClientSideException e) {
+            Assertions.assertEquals(ErrorSubStatus.LONG_POLL_TIME_OUT_SUB_STATUS, e.getErrorSubStatus());
+            Assertions.assertEquals(420, e.getStatusCode());
+
+            elapsedMs = System.currentTimeMillis() - startMs;
+            // NOTE: because the default poll timeout is 60-2 = 28s in iWF service
+            Assertions.assertTrue(elapsedMs >= 55 && elapsedMs <= 62, "expect to poll for 58 seconds, actual is %d " + elapsedMs);
             return;
         } catch (Exception e) {
-            Assertions.fail("no exception caught");
+            Assertions.fail("expected to catch ClientSideException");
         }
-
+        Assertions.fail("expected to catch ClientSideException");
     }
 
     @Test
