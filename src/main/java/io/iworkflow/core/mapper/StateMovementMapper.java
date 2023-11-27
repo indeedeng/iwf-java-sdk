0354a653787da3ef9a19c613f36d3266af1bbfa7
Quanzheng Long <qlong@indeed.com>
2023-11-27 15:06:02 -0800

    fix

--- src/main/java/io/iworkflow/core/mapper/StateMovementMapper.java
+++ src/main/java/io/iworkflow/core/mapper/StateMovementMapper.java
@@ -58,7 +58,7 @@ public class StateMovementMapper {
             String stateId = stateOptions.getExecuteApiFailureProceedStateId();
             final StateDef proceedStatDef = registry.getWorkflowState(workflowType, stateId);
             WorkflowStateOptions proceedStateOptions = proceedStatDef.getWorkflowState().getStateOptions();
-            if (stateOptions.getExecuteApiFailurePolicy() == ExecuteApiFailurePolicy.PROCEED_TO_CONFIGURED_STATE) {
+            if (proceedStateOptions.getExecuteApiFailurePolicy() == ExecuteApiFailurePolicy.PROCEED_TO_CONFIGURED_STATE) {
                 throw new WorkflowDefinitionException("nested failure handling is not supported. You cannot set a failure proceeding state on top of another failure proceeding state.");
             }
 
