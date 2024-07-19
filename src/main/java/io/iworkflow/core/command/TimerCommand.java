62a1cba83d8727b2be9079147b1f80274564ca01
Samuel Caçador <samuelmoreira27@gmail.com>
2024-07-19 16:01:43 +0100

    Make getDurationSeconds of long type

--- src/main/java/io/iworkflow/core/command/TimerCommand.java
+++ src/main/java/io/iworkflow/core/command/TimerCommand.java
@@ -7,18 +7,18 @@ import java.time.Duration;
 @Value.Immutable
 public abstract class TimerCommand implements BaseCommand {
 
-    public abstract int getDurationSeconds();
+    public abstract long getDurationSeconds();
 
     public static TimerCommand createByDuration(String commandId, Duration duration) {
         return ImmutableTimerCommand.builder()
                 .commandId(commandId)
-                .durationSeconds((int) duration.getSeconds())
+                .durationSeconds(duration.getSeconds())
                 .build();
     }
 
     public static TimerCommand createByDuration(Duration duration) {
         return ImmutableTimerCommand.builder()
-                .durationSeconds((int) duration.getSeconds())
+                .durationSeconds(duration.getSeconds())
                 .build();
     }
 }
