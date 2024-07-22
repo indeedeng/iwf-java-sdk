5fdc22ed05adde63e561206ead232c370a2751d5
Samuel Caçador <samuelmoreira27@gmail.com>
2024-07-22 10:39:01 +0100

    Replace firingUnixTimestamp with durationSeconds in TimerCommand (#229)

--- src/main/java/io/iworkflow/core/command/TimerCommand.java
+++ src/main/java/io/iworkflow/core/command/TimerCommand.java
@@ -7,18 +7,18 @@ import java.time.Duration;
 @Value.Immutable
 public abstract class TimerCommand implements BaseCommand {
 
-    public abstract int getFiringUnixTimestampSeconds();
+    public abstract long getDurationSeconds();
 
     public static TimerCommand createByDuration(String commandId, Duration duration) {
         return ImmutableTimerCommand.builder()
                 .commandId(commandId)
-                .firingUnixTimestampSeconds((int) (System.currentTimeMillis() / 1000 + duration.getSeconds()))
+                .durationSeconds(duration.getSeconds())
                 .build();
     }
 
     public static TimerCommand createByDuration(Duration duration) {
         return ImmutableTimerCommand.builder()
-                .firingUnixTimestampSeconds((int) (System.currentTimeMillis() / 1000 + duration.getSeconds()))
+                .durationSeconds(duration.getSeconds())
                 .build();
     }
 }
