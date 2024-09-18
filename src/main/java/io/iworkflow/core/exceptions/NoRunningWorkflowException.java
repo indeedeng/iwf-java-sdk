80134e8865faf96081e17962726de52e50889d23
lwolczynski <lwolczynski7@gmail.com>
2024-09-18 16:31:04 -0500

    IWF-117: Change NoRunningWorkflowException declaration

--- src/main/java/io/iworkflow/core/exceptions/NoRunningWorkflowException.java
+++ src/main/java/io/iworkflow/core/exceptions/NoRunningWorkflowException.java
@@ -2,7 +2,7 @@ package io.iworkflow.core.exceptions;
 
 import io.iworkflow.core.ClientSideException;
 
-public class NoRunningWorkflowException extends ClientSideException {
+public class NoRunningWorkflowException extends WorkflowNotExistsOrOpenException {
     public NoRunningWorkflowException(
             final ClientSideException exception) {
         super(exception);
