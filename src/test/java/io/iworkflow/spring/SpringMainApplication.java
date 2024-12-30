e484f619a83cf66be16f692f967783771a2b3863
lwolczynski <lwolczynski7@gmail.com>
2024-12-30 13:55:35 -0600

    IWF-438: Remove tests

--- src/test/java/io/iworkflow/spring/SpringMainApplication.java
+++ src/test/java/io/iworkflow/spring/SpringMainApplication.java
@@ -1,20 +1,14 @@
 package io.iworkflow.spring;
 
 import com.fasterxml.jackson.databind.Module;
-import io.iworkflow.integ.rpc.RpcWorkflowWithFinalRpc;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
-
 @SpringBootApplication
-@ComponentScan(
-        basePackages = {"io.iworkflow.gen", "io.iworkflow.gen.api", "io.iworkflow.spring.controller", "io.iworkflow.integ"},
-        excludeFilters={@ComponentScan.Filter(type=ASSIGNABLE_TYPE, value=RpcWorkflowWithFinalRpc.class)}
-)
+@ComponentScan(basePackages = {"io.iworkflow.gen", "io.iworkflow.gen.api", "io.iworkflow.spring.controller", "io.iworkflow.integ"})
 public class SpringMainApplication {
 
     public static void main(String[] args) {
